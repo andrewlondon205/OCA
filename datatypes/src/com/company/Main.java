e1c1bbda0ad7e784ed346411f32a172fab865380
Ramai Alejandria <vyasadev931@gmail.com>
2023-02-10 22:41:46 +0100

    primitive data types

---
+++ datatypes/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 25;
+        int secondNumber;
+
+        System.out.println(firstNumber);
+        secondNumber = 5;
+
+        System.out.println(secondNumber);
+
+        short day = 1, month = 2, year = 2000;
+
+        char firstNameInitial = 'A';
+        System.out.println(firstNameInitial);
+
+        boolean isThisTrue = true;
+        System.out.println(isThisTrue);
+
+        //int for = 0
+        int a1number = 0;
+
+        float myFloatNumber = 1.1f;
+        double myValue = 1.2;
+
+
+
+    }
+}
