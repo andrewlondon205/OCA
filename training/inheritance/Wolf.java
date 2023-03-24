42454382f8bf87cafd0bbdeb8c65084ea50e3bd8
Ramai Alejandria <vyasadev931@gmail.com>
2023-03-24 20:27:44 +0100

    inheritance exercises

---
+++ training/inheritance/Wolf.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+class Canine {
+
+    public double getAverageWeight () {
+        return 50;
+    }
+}
+
+public class Wolf extends Canine{
+
+    @Override
+    public double getAverageWeight() {
+        return super.getAverageWeight() + 20;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Canine().getAverageWeight());
+        System.out.println(new Wolf().getAverageWeight());
+    }
+}
