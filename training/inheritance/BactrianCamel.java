40e084da8fe4c4a146a75e66f3b5d7c2bd883353
Ramai Alejandria <vyasadev931@gmail.com>
2023-03-25 10:48:31 +0100

    inheritance exercises

---
+++ training/inheritance/BactrianCamel.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+
+class Camel {
+    protected Object getNumberOfHumps () {
+        return "Undefined";
+    }
+}
+
+public class BactrianCamel extends Camel {
+/*    private int getNumberOfHumps () {
+        return 2;
+    }*/
+}
