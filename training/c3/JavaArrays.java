305b4c1fbc11159ec1f4bf3ec69f2d8a28d21a9f
Ramai Alejandria <vyasadev931@gmail.com>
2023-04-19 19:22:56 +0200

    chapter three implementations

---
+++ training/c3/JavaArrays.java
@@ -0,0 +1,8 @@
+package c3;
+
+public class JavaArrays {
+    public static void main(String[] args) {
+
+    }
+
+}
--- training/c3/StringClass.java
+++ training/c3/StringClass.java
@@ -1,6 +1,7 @@
 package c3;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class StringClass {
     public static void main(String[] args) {
@@ -181,6 +182,17 @@ public class StringClass {
 
         String trimmedString = " \t a b c\n".trim();
         System.out.println(trimmedString);
+
+        /**
+         * METHOD CHAINING
+         * - It is common to call multiple methods on the same String
+         */
+
+        String beginning = "Katy Perry is an outstanding singer and performer";
+        String result = beginning.trim().toLowerCase().replace("y","ii").replace("s","$$");
+        System.out.println(result);
+
+
     }
 }
 
@@ -197,4 +209,184 @@ final class Immutable {
     public String getS() {
         return s;
     }
+}
+
+class StringBuilderClass {
+
+    public static void main(String[] args) {
+        //   gettingStarted();
+        //  characterAtIndex();
+        // append();
+        //insert();
+       //deleteMethods();
+        reverse();
+    }
+
+    public static void gettingStarted() {
+
+        StringBuilder alpha= new StringBuilder();
+        for (char current = 'a'; current <= 'z'; current ++) {
+            alpha.append(current);
+        }
+
+        System.out.println(alpha);
+
+        StringBuilder sb = new StringBuilder("start");
+        sb.append("+middle");
+
+        StringBuilder same = sb.append("+end");
+
+        System.out.println(same);
+
+        StringBuilder a = new StringBuilder("abc");
+        StringBuilder b = a.append("de");
+
+        b = b.append("f").append("g");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+
+        /**
+         * Three ways to construct a StringBuilder
+         */
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder("animal");
+        StringBuilder sb3 = new StringBuilder(10);
+
+        /**
+         * Size = refers to the number of characters currently present in the sequence
+         * Capacity = refers to the number of characters that the sequence can hold.
+         *
+         * - Initial capacity of StringBuilder is 16, unless the programmer happens to choose one in the beginning
+         * - Since StringBuilder is mutable, the capacity can change.
+         */
+
+        StringBuilder s1 = new StringBuilder(4); //initial capacity is 4
+        s1.append("KatyPerry/EyeOfTiger");
+
+        System.out.println("Current Object State: " + s1);
+        System.out.println("Object Size: " + s1.length());
+        System.out.println("Object Capacity: " + s1.capacity());
+
+    }
+
+    public static void characterAtIndex() {
+        StringBuilder sb = new StringBuilder("animals");
+        String sub = sb.substring(sb.indexOf("a"), sb.indexOf("al"));
+
+        int len = sb.length();
+        char ch = sb.charAt(6);
+        System.out.println(sub + " " + len + " " + ch);
+        //NOTE: substring method from StringBuilder returns a string, not a stringbuilder;
+    }
+
+    public static void append() {
+        /**
+         * it adds the parameter to the StringBuilder and returns a reference to the current StringBuilder.
+         *
+         * StringBuilder append (String str)
+         */
+        StringBuilder sb = new StringBuilder().append(1).append('c');
+        sb.append("-").append(true).append(new Date());
+        System.out.println(sb);
+    }
+
+    public static void insert() {
+
+        /**
+         * - The insert() method adds characters to the StringBuilder at the requested index and
+         *   returns a reference to the current StringBuilder.
+         * - Just like append(), there are lots of method signatures for different types. Here’s one:
+         *
+         * - StringBuilder insert(int offset, String str)
+         */
+
+        StringBuilder sb = new StringBuilder("animals");
+
+        sb.insert(7,"-");
+        sb.insert(0,"-");
+        sb.insert(4,"-");
+        sb.insert(8,"42");
+
+        //The method does throw an exception if we provide a negative index or invalid index (greater than length)
+        sb.insert(5,new Date());
+        sb.insert(6,false);
+
+        System.out.println(sb.length());
+        System.out.println(sb);
+
+    }
+
+    public static void deleteMethods () {
+
+        /**
+         * - The delete() method is the opposite of the insert() method. It removes characters from
+         *   the sequence and returns a reference to the current StringBuilder.
+         *
+         * - The deleteCharAt() method is convenient when you want to delete only one character.
+         *   The method signatures are as follows:
+         *
+         * - StringBuilder delete(int start, int end)
+         * - StringBuilder deleteCharAt(int index)
+         */
+
+        StringBuilder sb = new StringBuilder ("EyeOfTheTiger");
+
+        sb.delete(6,21);// does not throw an exception
+        //sb.deleteCharAt(-1); throws exception
+
+        System.out.println(sb);
+
+        /**
+         * NOTES
+         *
+         * deleteCharAt()
+         * - it is used to delete a single character from the StringBuilder.
+         * - throws an exception if we provide values greater than length or negative values.
+         *
+         * delete(int start, int end)
+         *
+         * - it is used to delete multiple characters at once given a range of indices.
+         * - throws an exception if one of the indexes is negative (start/end or both)
+         * - the method will NOT throw an exception if we provide a valid starting index and an ending index that
+         *   is greater than length:
+         *
+         *   example:
+         *   StringBuilder sb = new StringBuilder("hello");
+         *   sb.delete(2,250) // outputs he because beginning index is inclusive
+         *
+         *
+         */
+    }
+
+    public static void reverse() {
+
+        /**
+         * The reverse() method does just what it sounds like: it reverses the characters in the sequences and returns a
+         * reference to the current StringBuilder. The method signature is as follows:
+         *
+         * - StringBuilder reverse()
+         *
+         */
+
+        StringBuilder sb = new StringBuilder("KatyPerry");
+        sb.reverse();
+
+        System.out.println(sb);
+    }
+
+    public static void toStringMethod() {
+        /**
+         * The last method converts a StringBuilder into a String. The method signature is as follows:
+         * - String toString()
+         *
+         * - Often StringBuilder is used internally for performance purposes but the end result
+         *   needs to be a String. For example, maybe it needs to be passed to another method that is
+         *   expecting a String.
+         */
+
+    }
+
+
+
 }
