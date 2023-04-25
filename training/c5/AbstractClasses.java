372b64645a019f72d554b4db14e38b901c17fd3e
Ramai Alejandria <vyasadev931@gmail.com>
2023-04-25 18:43:58 +0200

    chapter four

---
+++ training/c5/AbstractClasses.java
@@ -0,0 +1,245 @@
+package c5;
+
+/**
+ * UNDERSTANDING ABSTRACT CLASSES
+ *
+ *
+ * - An abstract class is a class that is marked with the abstract keyword and cannot be instantiated.
+ * - An abstract method is a method marked with the abstract keyword defined in an abstract class, for which no
+ *   implementation is provided in the class in which it is declared.
+ * -
+ */
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class AlphaAnimal {
+    protected int age;
+    public void eat () {
+        System.out.println("Animal is eating");
+    }
+
+    public abstract String getName();
+}
+
+class Swan extends AlphaAnimal {
+    public String getName () {
+        return "Swan";
+    }
+
+}
+
+/**
+ * DEFINING AN ABSTRACT CLASS
+ *
+ * - The previous sample code illustrates a number of important rules about abstract classes.
+ * - For example, an abstract class may include nonabstract methods and variables, as you saw with the variable age and
+ *   the method eat().
+ * - In fact, an abstract class is not required to include any abstract methods. For example, the following code compiles
+ *   without issue even though it doesn’t define any abstract methods:
+ *
+ * - Although an abstract class doesn’t have to implement any abstract methods, an abstract method may only be defined
+ *   in an abstract class.
+ */
+
+abstract class Cow {  }
+class Chicken {
+  //  public abstract void peck() ; causes compilation failure
+}
+
+/**
+ * Exam creators are fond  of questions like this one, which mixes non-abstract classes with abstract methods.
+ */
+
+abstract class Turtle {
+    //public abstract void swim() {} does not compile because abstract methods cannot have a body
+    //public abstract int getAge() {return 10; } does not compile
+}
+
+/**
+ * - Although you can’t provide a default implementation to an abstract method in an abstract class, you can still define
+ *   a method with a body—you just can’t mark it as abstract.
+ *
+ * - As long as you do not mark it as final, the subclass still has the option to override it, as explained in the previous
+ *   section.
+ *
+ * - Next, we note that an abstract class cannot be marked as final for a somewhat obvi- ous reason. By definition, an
+ *   abstract class is one that must be extended by another class to be instantiated, whereas a final class can’t be
+ *   extended by another class.
+ *
+ * - By marking an abstract class as final, you’re saying the class can never be instantiated, so the compiler refuses
+ *   to process the code. For example, the following code snippet will not compile:
+ */
+
+// final abstract class Tortoise { } does not compile
+
+/**
+ * ABSTRACT METHODS CANNOT BE MARKED AS FINAL
+ *
+ * - Likewise, an abstract method may not be marked as final for the same reason that an abstract class may not be
+ *   marked as final.
+ * - Once marked as final, the method can never be overridden in a subclass, making it impossible to create a concrete
+ *   instance of the abstract class.
+ *
+ */
+
+abstract class Goat {
+    // public abstract final void chew (); does not compile
+}
+
+/**
+ *
+ * ABSTRACT METHODS CANNOT BE MARKED AS PRIVATE
+ *
+ * - Finally, a method may not be marked as both abstract and private. This rule makes sense if you think about it.
+ * - How would you define a subclass that implements a required method if the method is not accessible by the subclass
+ *   itself?
+ *
+ * - The answer is you can’t, which is why the compiler will complain if you try to do the following:
+ */
+
+abstract class MyWhale {
+    // private abstract void sing (); does not compile
+    protected abstract void sing();
+}
+
+class HumpBackWhale extends MyWhale {
+/*    private void sing () {
+        System.out.println("Humpback Whale is singing"); does not compile because method visiblity has been reduced
+    }*/
+
+    protected void sing () {
+        System.out.println("WHOOOOO");
+    }
+}
+
+/**
+ * CREATING CONCRETE CLASSES
+ *
+ * - When working with abstract classes, it is important to remember that by themselves, they cannot be instantiated and
+ *   therefore do not do much other than define static variables and methods.
+ *
+ * - For example, the following code will not compile as it is an attempt to instantiate an abstract class.
+ */
+
+abstract class Eel {
+    public static void main(String[] args) {
+      //  final Eel eel = new Eel() ; does not compile because abstract classes cannot be instantiated
+    }
+}
+
+/**
+ * - An abstract class becomes useful when it is extended by a concrete subclass. A concrete class is the first non-abstract
+ *   subclass that extends an abstract class and is required to implement all inherited abstract methods.
+ *
+ * - When you see a concrete class extending an abstract class on the exam, check that it implements all of the required
+ *   abstract methods.
+ */
+
+abstract class Band {
+    public abstract String getName ();
+}
+
+class RockBand extends Band {
+    public String getName () {
+        return "ACDC";
+    }
+}
+
+/**
+ * - First, note that Band is marked as abstract and RockBand is not. In this example, RockBand is considered the first
+ *   concrete subclass of Animal.
+ *
+ * - Second, since RockBand is the first concrete subclass, it must implement all inherited abstract methods, getName(),
+ *
+ * - The key point is that the first class to extend the nonabstract class must implement all inherited abstract methods.
+ *   For example, the following variation will also not compile:
+ */
+
+abstract class Computer {
+    public abstract String getName();
+}
+
+/*class Laptop extends Computer {
+does not compile
+}*/
+
+abstract class Laptop extends Computer {
+    // it compiles if we mark the class as abstract
+}
+
+
+
+class NoteBook extends Laptop {
+    public String getName() {
+        return "Macbook Pro";
+    }
+}
+
+// Even though a second subclass Notebook implements the abstract method getName(), the first concrete subclass Laptop doesn't
+
+/**
+ * EXTENDING AN ABSTRACT CLASS
+ *
+ * - Let’s expand our discussion of abstract classes by introducing the concept of extending an abstract class with
+ *   another abstract.
+ */
+
+abstract class Food {
+    public abstract String getName ();
+}
+
+/*class VeganFood extends Food {
+
+}*/
+
+abstract class DairyFood extends Food {
+
+}
+
+/**
+ * - In this example, we have an abstract class Food, with a concrete subclass VeganFood that doesn't compile since it
+ *   doesn't implement a getName() method.
+ *
+ * - We also have an abstract class DairyFood, which like VeganFood extends Food and doesn't provide an implementation
+ *   for getName().
+ *
+ * - In this situation, DairyFood does compile because it is marked as abstract while VeganFood doesn't compile
+ *
+ * - It follows, then, that a concrete class that extends an abstract class must implement all inherited abstract methods.
+ */
+
+abstract class LandAnimal {
+    public abstract String getName();
+}
+
+abstract class BigCat extends LandAnimal {
+    public abstract void roar();
+}
+
+class TheTiger extends BigCat {
+    public String getName() {
+        return "Tiger";
+    }
+
+    public void roar (){
+        System.out.println("The Tiger lets out a loud ROAR");
+    }
+}
+
+/**
+ * - BigCat extends Animal, but it is marked as abstract; therefore, it is not required to provide an implementation for
+ * getName() method.
+ *
+ * - The tiger class is not marked as abstract, and as first concrete subclass, it must implement all inherited abstract
+ *   methods not defined in a parent class.
+ *
+ * EXCEPTION
+ *
+ * - A concrete subclass is not required to provide an implementation for an abstract method if an intermediate abstract
+ *   class provides the implementation. For example, take a look at the following variation on our previous example:
+ *
+ */
--- training/c5/Inheritance.java
+++ training/c5/Inheritance.java
@@ -256,3 +256,534 @@ class Elephant extends Mammal {
  *       and therefore doesn’t compile.
  */
 
+/**
+ * CALLING CONSTRUCTORS
+ *
+ * In Java, the parent constructor is always executed before the child constructor. For example, try to determine
+ * what the following code outputs:
+ */
+
+class Primate {
+    public Primate () {
+        System.out.println("Primate");
+    }
+}
+
+class Ape extends Primate {
+    public Ape () {
+        System.out.println("Ape");
+    }
+}
+
+class Chimpanzee extends Ape {
+    public static void main(String[] args) {
+        new Chimpanzee();
+    }
+}
+
+/**
+ * CALLING INHERITED CLASS MEMBERS
+ * - Java classes may use any public or protected member of the parent class, including methods, primitives, or object
+ *   references.
+ *
+ * - If the parent class and child class are part of the same package, the child class may also use any default members
+ *   defined in the parent class.
+ *
+ * - Finally, a child class may never access a private member of the parent class, at least not through any direct reference.
+ *
+ * - As you saw in the first example in this chapter, a private member age may be accessed indirectly via a public or
+ *   protected method.
+ *
+ * - To reference a member in a parent class, you can just call it directly, as in the following example with the output
+ *   function displaySharkDetails():
+ */
+
+class Fish {
+    protected int size;
+    private int age;
+    public Fish (int age) {
+        this.age = age;
+    }
+
+    public int getAge () {
+        return age;
+    }
+}
+
+class Shark extends Fish {
+    private int numberOfFins = 8;
+    public Shark(int age) {
+        super(age);
+        this.size = 4;
+    }
+
+    public void displaySharkDetails () {
+        System.out.print("Shark with age:" + getAge() );
+        System.out.print(" and " + size + " meters long");
+        System.out.print("with " + numberOfFins + " fins");
+
+        System.out.print("Shark with age:" + this.getAge() );
+        System.out.print(" and " + this.size + " meters long");
+        System.out.print("with " + this.numberOfFins + " fins");
+
+        System.out.print("Shark with age:" + super.getAge() );
+        System.out.print(" and " + super.size + " meters long");
+        System.out.print("with " + this.numberOfFins + " fins");
+        // System.out.print("with " + super.numberOfFins + " fins"); does not compile because numberOfFins doesn't exist in the parent class
+
+    }
+}
+
+/**
+ * INHERITING METHODS
+ *
+ * - Inheriting a class grants us access to the public and protected members of the parent class, but also sets the stage
+ *   for collisions between methods defined in both the parent class and the subclass.
+ *
+ * - In this section, we’ll review the rules for method inheritance and how Java handles such scenarios.
+ *
+ * METHOD OVERRIDING
+ *
+ * - What if there is a method defined in both the parent and child class? For example, you may want to define a new
+ *   version of an existing method in a child class that makes use of the definition in the parent class.
+ *
+ * - In this case, you can override a method by declaring a new method with the signature and return type as
+ *   the method in the parent class.
+ *
+ * - When you override a method, you may reference the parent version of the method using the super keyword. In this manner,
+ *   the keywords this and super allow you to select between the current and parent version of a method, respectively.
+ * -
+ */
+
+class Canine {
+
+    public double getAverageWeight () {
+        return 50;
+    }
+
+}
+
+class Wolf extends Canine{
+    public double getAverageWeight () {
+        return super.getAverageWeight() + 20;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new Canine().getAverageWeight());
+        System.out.println(new Wolf().getAverageWeight());
+
+        //without super keyword, we would go in infinite recursion
+    }
+}
+
+/**
+ * OVERRIDING RULES
+ *
+ *  1. The method in the child class must have the same signature as the method in the parent class.
+ *  2. The method in the child class must be at least as accessible or more accessible than the method in the parent class.
+ *  3. The method in the child class may not throw a checked exception that is new or broader than the class of any
+ *     exception thrown in the parent class method.
+ *  4. If the method returns a value, it must be the same or a subclass of the method in the parent class, known as
+ *     covariant return types.
+ */
+
+/**
+ * OVERRIDING VS OVERLOADING
+ *
+ * - Overloading a method and overriding a method are similar in that they both involve redefining a method using the
+ *   same name.
+ *
+ * - They differ in that an overloaded method will use a different signature than an overridden method.
+ * - This distinction allows overloaded methods a great deal more freedom in syntax than an overridden method would have.
+ *
+ *
+ */
+
+class Bird {
+    public void fly () {
+        System.out.println("Bird is flying");
+    }
+    public void eat(int food) {
+        System.out.println("Bird is eating " + food + " units of food");
+    }
+}
+
+class Eagle extends Bird {
+    public int fly (int height) {
+        System.out.println("Bird is flying at " + height + " meters");
+        return height;
+    }
+
+/*    public int eat(int food) {
+        System.out.println("Bird is eating " + food + " units of food"); does not compile because return type is not covariant
+        return food;
+    }*/
+
+    public void eat (int food) {
+        System.out.println("Bird is eating " + food + " units of food");
+    }
+}
+
+class Camel {
+    protected String getNumberOfHumps() {
+        return "Undefined";
+    }
+}
+
+class BactrianCamel extends Camel {
+    /* private int getNumberOfHumps () { // does not compile because the return type is not covariant, and it's less vissible
+         return 2;
+    }*/
+    public String getNumberOfHumps() {
+        return "Overriden method";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Camel().getNumberOfHumps()); // undefined
+        Camel c = new BactrianCamel();
+        System.out.println(c.getNumberOfHumps()) ; // Overridden method
+    }
+}
+
+class InsufficientDataException extends Exception  { }
+
+class Reptile {
+    protected boolean hasLegs() throws InsufficientDataException {
+        throw new InsufficientDataException();
+    }
+
+    protected double getWeight () throws Exception  {
+        return 2;
+    }
+
+    protected double getHeight () throws InsufficientDataException {
+        return 2;
+    }
+
+    protected int getLength () {
+        return 10;
+    }
+}
+
+class Snake extends Reptile {
+    protected boolean hasLegs () {
+        return false;
+    }
+
+    protected double getWeight () throws InsufficientDataException {
+        return 2;
+    }
+
+
+/*    protected double getHeight () throws Exception {
+        return 4;
+    }
+
+    protected int getLength() throws InsufficientDataException {
+        return 10;
+
+        these methods don't compile because they violate the rules for overriding methods
+    }*/
+
+    protected double getHeight() throws InsufficientDataException {
+        return 21;
+    }
+
+    protected int getLength () {
+        return 124;
+    }
+}
+
+/**
+ * RE-DECLARING PRIVATE METHODS
+ *
+ * - The previous section defined the behavior if you override a public or protected method in the class. Now let’s expand
+ *   our discussion to private methods.
+ *
+ * - In Java, it is not possible to override a private method in a parent class since the parent method is not accessible
+ *   from the child class.
+ *
+ * - Just because a child class doesn’t have access to the parent method, doesn’t mean the child class can’t define its
+ *   own version of the method. It just means, strictly speaking, that the new method is not an overridden version of
+ *   the parent class’s method.
+ *
+ * - Java permits you to redeclare a new method in the child class with the same or modified signature as the method
+ *   in the parent class.
+ *
+ * - This method in the child class is a separate and independent method, unrelated to the parent version’s method, so
+ *   none of the rules for overriding methods are invoked.
+ *
+ * - For example, let’s return to the Camel example we used in the previous section and show two related classes that
+ *   define the same method:
+ */
+
+class Whale {
+    private String getName () {
+        return "A random Whale";
+    }
+}
+
+class BlueWhale extends Whale {
+    private String getName () {
+        return "A blue Whale";
+    }
+}
+
+//Code compiles without issue. The method getName() has not been overridden, but hidden.
+//If the method in the parent class were public, protected or package-private, then the code would not compile
+
+/**
+ * HIDING STATIC METHODS
+ *
+ * - A hidden method occurs when a child class defines a static method with the same name and signature as a static
+ *   method defined in a parent class.
+ *
+ * - Method hiding is similar but not exactly the same as method overriding. First, the four previous rules for
+ *   overriding a method must be followed when a method is hidden.
+ *
+ * - In addition, a new rule is added for hiding a method, namely that the usage of the static keyword must be the same
+ *   between parent and child classes. The following list summarizes the five rules for hiding a method:
+ *
+ * 1. The method in the child class must have the same signature as the method in the parent class.
+ *
+ * 2. The method in the child class must be at least as accessible or more accessible than the method in the parent class.
+ *
+ * 3. The method in the child class may not throw a checked exception that is new or broader than the class of any exception
+ *    thrown in the parent class method.
+ *
+ * 4. If the method returns a value, it must be the same or a subclass of the method in the parent class, known as
+ *    covariant return types.
+ *
+ * 5. The method defined in the child class must be marked as static if it is marked as static in the parent class
+ *    (method hiding). Likewise, the method must not be marked as static in the child class if it is not marked as static
+ *    in the parent class (method overriding).
+ *
+ * - Note that the first four are the same as the rules for overriding a method.
+ */
+
+class Bear {
+    public static void eat() {
+        System.out.println("Bear is eating");
+    }
+
+    public static void sneeze() {
+        System.out.println("Bear is sneezing");
+    }
+
+    public void hibernate () {
+        System.out.println("Bear is hibernating");
+    }
+}
+
+class Gryzzly extends Bear {
+    public static void eat() {
+        System.out.println("Gryzzly bear is chewing");
+    }
+
+/*
+    public void sneeze () {
+        System.out.println("Gryzzly bear sneezes quietly");
+    }
+
+    public static void hibernate () {
+        System.out.println("Grizzly bear is going to sleep");
+    }
+
+    Code doesnot compile because it violates the 5th rule of static method hiding
+*/
+
+    public static void sneeze() {
+        System.out.println("Gryzzly bear sneezes quietly");
+    }
+
+    public void hibernate() {
+        System.out.println("Grizzly bear is going to sleep");
+    }
+    public static void main(String[] args) {
+        Gryzzly.eat();
+    }
+}
+
+/**
+ * OVERRIDING VS HIDING METHODS
+ *
+ * - In our description of hiding of static methods, we indicated there was a distinction between overriding and hiding methods.
+ *
+ * - Unlike overriding a method, in which a child method replaces the parent method in calls defined in both the parent
+ *   and child, hidden methods only replace parent methods in the calls defined in the child class.
+ *
+ * - At runtime the child version of an overridden method is always executed for an instance regardless of whether the
+ *   method call is defined in a parent or child class method.
+ *
+ * - In this manner, the parent method is never used unless an explicit call to the parent method is referenced, using
+ *   the syntax ParentClassName.method().
+ *
+ * - Alternatively, at runtime the parent version of a hidden method is always executed if the call to the method is
+ *   defined in the parent class. Let’s take a look at an example:
+ */
+
+class Marsupial {
+    public static boolean isBiped () {
+        return false;
+    }
+
+    public void getMarsupialDescription () {
+        System.out.println("Marsupial walks on two legs " + isBiped());
+    }
+}
+
+//Method hiding example
+class Kangaroo extends Marsupial {
+    public static boolean isBiped() {
+        return true;
+    }
+
+    public void getKangarooDescription() {
+        System.out.println("Kangaroo hops on two legs: " + isBiped());
+    }
+
+    public static void main(String[] args) {
+        Kangaroo joey = new Kangaroo();
+        joey.getMarsupialDescription();
+        joey.getKangarooDescription();
+
+        Marsupial map = new Marsupial();
+        System.out.println(Marsupial.isBiped());
+        System.out.println(joey.isBiped());
+    }
+}
+
+/**
+ * - Notice that isBiped() returns false in the parent class and true in the child class.
+ *   In the first method call, the parent method getMarsupialDescription() is used.
+ *
+ * - The Marsupial class only knows about isBiped() from its own class definition, so it outputs false.
+ *
+ * - In the second method call, the child executes a method of isBiped(), which hides the parent method’s version and
+ *   returns true.
+ *
+ *
+ */
+
+class Feline {
+    public boolean isQuadruped () {
+        return false;
+    }
+
+    public void getFelineDescription () {
+        System.out.println("Feline walks on four legs: " + isQuadruped());
+    }
+}
+
+class Tiger extends Feline {
+    public boolean isQuadruped () {
+        return true;
+    }
+
+    public void getTigerDescription () {
+        System.out.println("Tiger walks on four legs: " + isQuadruped());
+    }
+
+    public static void main(String[] args) {
+        Tiger t = new Tiger();
+        t.getFelineDescription();
+        t.getTigerDescription();
+    }
+}
+
+/**
+ * - In this example, the isBiped() method is overridden, not hidden, in the child class.
+ *
+ * - Therefore, it is replaced at runtime in the parent class with the call to the child class’s method.
+ *
+ * - Make sure you understand these examples as they show how hidden and overridden methods are fundamentally
+ *   different. This example makes uses of polymorphism, which we’ll discuss later in this chapter.
+ *
+ */
+
+/**
+ * CREATING FINAL METHODS
+ * - We conclude our discussion of method inheritance with a somewhat self-explanatory rule:
+ * - final methods cannot be overridden. If you recall our discussion of modifiers from Chapter 4, you can create a
+ *   method with the final keyword.
+ * - By doing so, though, you for- bid a child class from overriding this method.
+ * - This rule is in place both when you override a method and when you hide a method.
+ * - In other words, you cannot hide a static method in a parent class if it is marked as final.
+ */
+
+class TheBird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+}
+
+class Penguin extends TheBird {
+    /*  public final boolean hasFeathers () {
+        return false;
+        Does not compile because final methods cannot be overridden
+    }*/
+}
+
+/**
+ * INHERITING VARIABLES
+ *
+ * - As you saw with method overriding, there were a lot of rules when two methods have the same signature and are
+ *   defined in both the parent and child classes.
+ * - Luckily, the rules for variables with the same name in the parent and child classes are a lot simpler, because Java
+ *   doesn’t allow variables to be overridden but instead hidden.
+ *
+ * HIDING VARIABLES
+ *
+ * - When you hide a variable, you define a variable with the same name as a variable in a parent class. This creates
+ *   two copies of the variable within an instance of the child class: one instance defined for the parent reference and
+ *   another defined for the child reference.
+ *
+ * - As when hiding a static method, you can’t override a variable; you can only hide it
+ *
+ * - Also similar to hiding a static method, the rules for accessing the parent and child variables are quite similar.
+ * - If you’re referencing the variable from within the parent class, the variable defined in the parent class is used
+ * - Alternatively, if you’re referencing the variable from within a child class, the variable defined in the child class
+ *   is used.
+ * - Likewise, you can reference the parent value of the variable with an explicit use of the super keyword.
+ *
+ */
+
+class Rodent {
+    protected int tailLength = 4;
+    public void getRodentDetails () {
+        System.out.println("[parentTail="+tailLength+"]");
+    }
+}
+
+class Mouse extends Rodent{
+    protected int tailLength = 8;
+    public void getMouseDetails () {
+        System.out.println("[tail=" + tailLength + " ,parentTail=" + super.tailLength + "]");
+    }
+
+    public static void main(String[] args) {
+        Mouse mouse = new Mouse();
+        mouse.getRodentDetails();
+        mouse.getMouseDetails();
+    }
+}
+
+//VARIABLE HIDING
+
+class MyAnimal {
+    public int length = 2;
+}
+
+class JellyFish extends MyAnimal {
+    public int length = 5;
+
+    public static void main(String[] args) {
+        JellyFish jellyFish = new JellyFish();
+        MyAnimal myAnimal = new JellyFish();
+
+        System.out.println(jellyFish.length);
+        System.out.println(myAnimal.length);
+    }
+}
+
