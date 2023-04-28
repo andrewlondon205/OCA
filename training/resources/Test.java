ef0f1beafaedd2407883e37d7b2a22744683298c
Ramai Alejandria <vyasadev931@gmail.com>
2023-04-28 06:58:09 +0200

    modifications

--- training/resources/Test.java
+++ training/resources/Test.java
@@ -28,3 +28,17 @@ public class Test {
     }
 
 }
+
+interface ILog {
+    default void log() {
+        System.out.println("ILog");
+    }
+}
+
+abstract class Log {
+    public static void log() {
+        System.out.println("Log");
+    }
+}
+
+class MyLogger extends Log implements ILog {}
